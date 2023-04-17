539b77ae92687ddca3371d6a3cd242fad0412b3d
Yossi Karasik <yosikari@gmail.com>
2023-04-17 16:46:38 +0300

    Add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Nunito } from 'next/font/google'
 
 import './globals.css'
+import Navbar from './components/navbar/navbar'
 
 export const metadata = {
   title: 'Airbmb',
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
 
 export default function Home() {
-  return <div>Hello AirBNB</div>
+  return <div className="text-rose-500 text-2xl">Hello AirBNB</div>
 }
