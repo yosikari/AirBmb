b1026f69a5e6792c6b4ce133afc18150afb448f2
Yossi Karasik <yosikari@gmail.com>
2023-04-19 18:33:00 +0300

    Render listings cards on homepage from mongoDB.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import getCurrentUser from "./actions/get-current-user"
+import getListings from "./actions/get-listings"
 import ClientOnly from "./components/client-only"
 import Container from "./components/container"
 import EmptyState from "./components/empty-state"
+import ListingCard from "./components/listings/listing-card"
 
-export default function Home() {
-  const isEmpty = true
+export default async function Home() {
+  const listings = await getListings()
+  const currentUser = await getCurrentUser()
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
@@ -17,7 +21,11 @@ export default function Home() {
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         <div>
-          My future listings:
+          {listings.map((listing: any) => <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />)}
         </div>
       </div>
     </Container>
