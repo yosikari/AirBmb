033be5ad5f419db7bbdf943b3105d71c623c564e
Yossi Karasik <yosikari@gmail.com>
2023-04-19 14:21:45 +0300

    Add rent-modal step2 (location selection, and map)

--- app/components/modals/rent-modal.tsx
+++ app/components/modals/rent-modal.tsx
@@ -8,6 +8,8 @@ import Modal from './modal'
 import Heading from '../heading'
 import { categories } from '../navbar/categories'
 import CategoryInput from '../inputs/category-input'
+import CountrySelect from '../inputs/country-select'
+import dynamic from 'next/dynamic'
 
 enum STEPS {
     CATEGORY = 0,
@@ -46,6 +48,11 @@ const RentModal = () => {
     })
 
     const category = watch('category')
+    const location = watch('location')
+
+    const Map = useMemo(() => dynamic(() => import('../map'), {
+        ssr: false
+    }), [location])
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -56,11 +63,11 @@ const RentModal = () => {
     }
 
     const onBack = () => {
-        setStep((val) => val - 1)
+        setStep((value) => value - 1)
     }
 
     const onNext = () => {
-        setStep((val) => val + 1)
+        setStep((value) => value + 1)
     }
 
     const actionLabel = useMemo(() => {
@@ -68,7 +75,7 @@ const RentModal = () => {
         return 'Next'
     }, [step])
 
-    const secondaryLabel = useMemo(() => {
+    const secondaryActionLabel = useMemo(() => {
         if (step === STEPS.CATEGORY) return undefined
         return 'Back'
     }, [step])
@@ -94,13 +101,31 @@ const RentModal = () => {
         </div>
     )
 
+    if (step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className='flex flex-col gap-8'>
+                <Heading
+                    title='Where is your place located?'
+                    subtitle='Help guests find you!'
+                />
+                <CountrySelect
+                    value={location}
+                    onChange={(value) => setCustomValue('location', value)}
+                />
+                <Map
+                    center={location?.coordinates}
+                />
+            </div>
+        )
+    }
+
     return <Modal
         title={"Airbnb your home"}
         isOpen={rentModal.isOpen}
         onClose={rentModal.onClose}
-        onSubmit={rentModal.onClose}
+        onSubmit={onNext}
         actionLabel={actionLabel}
-        secondaryActionLabel={secondaryLabel}
+        secondaryActionLabel={secondaryActionLabel}
         secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
         body={bodyContent}
     />
