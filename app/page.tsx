32c66acd14ff57759fbc82cf62bea3490033fd46
Yossi Karasik <yosikari@gmail.com>
2023-04-19 17:23:51 +0300

    Add empty-state cmp (show on empty filter results)

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,25 @@
+import ClientOnly from "./components/client-only"
+import Container from "./components/container"
+import EmptyState from "./components/empty-state"
 
 export default function Home() {
-  return <span className="loader"></span>
+  const isEmpty = true
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
+
+  return <ClientOnly>
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        <div>
+          My future listings:
+        </div>
+      </div>
+    </Container>
+  </ClientOnly>
 }
