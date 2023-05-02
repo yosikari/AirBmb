3b9c27d4db5e1e541cf0d06bc6fdbae509646429
Yossi Karasik <yosikari@gmail.com>
2023-05-02 11:23:50 +0300

    Fix deploy err.

--- app/components/modals/search-modal.tsx
+++ app/components/modals/search-modal.tsx
@@ -42,7 +42,7 @@ const SearchModal = () => {
 
   const Map = useMemo(() => dynamic(() => import('../map'), { 
     ssr: false 
-  }), [location]);
+  }), [location])
 
   const onBack = useCallback(() => {
     setStep((value) => value - 1)
